73d1032510c24e1af04e6b919750b2a5e075bc28
oh29oh29 <hyukjae89@gmail.com>
2020-03-18 20:52:14 +0900

    프로젝트 초기화

---
+++ src/main/java/pe/oh29oh29/myspringreference/MySpringReferenceApplication.java
@@ -0,0 +1,13 @@
+package pe.oh29oh29.myspringreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringReferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringReferenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/oh29oh29/myspringreference/MySpringReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.oh29oh29.myspringreference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringReferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
