e7a0e66e470e2bdc80f4053efd733757846b0550
oh29oh29 <hyukjae89@gmail.com>
2020-03-20 21:38:41 +0900

    [ExceptionHandler] @ExceptionHandler

--- exception-handler/src/main/java/pe/oh29oh29/exceptionhandler/case01/controller/HelloController.java
+++ exception-handler/src/main/java/pe/oh29oh29/exceptionhandler/case01/controller/HelloController.java
@@ -1,22 +1,25 @@
 package pe.oh29oh29.exceptionhandler.case01.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pe.oh29oh29.exceptionhandler.case01.service.HelloService;
 
 @Controller
+@RequestMapping("/case01")
 public class HelloController {
 
-    @Autowired
-    private HelloService helloService;
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
 
     @GetMapping
     public void hello() {
         helloService.hello();
     }
-
 
     /**
      * the @ExceptionHandler annotated method is only active for that particular Controller, not globally for the entire application.
